cd1af39b27fe93b75f3c8a505a88e8f7e69b33f7
Nelson Ramirez <xhyskoax@gmail.com>
2023-02-03 19:31:53 -0300

    parte 2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const moment = require('moment');
+const randomWords = require('random-words');
+
+
 moment.locale("ES"); 
 
 const app = express()
@@ -8,14 +11,23 @@ app.get('/', function (req, res) {
   res.send(`
     <ul>
       <li>Día: ${moment().format('dddd')}</li>
-      <li>Número día: </li>
-      <li>Mes: </li>
+      <li>Número día: ${moment().format('D')}</li>
+      <li>Mes: ${moment().format('MMMM')}</li>
       <li>Año: </li>
       <li>Hora: </li>
       <li>Minutos: </li>
       <li>Segundos: </li>
     </ul>
   `)
+})
+
+app.get('/azar', function(req, res){
+  console.log(randomWords())
+  res.send(`<h1>${randomWords({exactly: 1, maxLength: 10})}</h1>`)
+})
+
+app.put('/azar', function(req, res){
+  res.send("azar número")
 })
 
 app.listen(3000,() => console.log("Servidor escuchando en http://localhost:3000"))
